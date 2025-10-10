4e4603fc566951d20b68d70b5428283b186f8a0b
nickruden <1nikitos.ru2018@gmail.com>
2025-10-10 18:33:27 +0300

    вёрстка

---
+++ assets/scripts/fancybox-init.js
@@ -0,0 +1,12 @@
+Fancybox.bind("[data-fancybox]", {
+  Carousel: {
+    Thumbs: false,
+    Toolbar: {
+      display: {
+        left: ["counter"],
+        middle: [],
+        right: ["close"],
+      },
+    },
+  },
+});
